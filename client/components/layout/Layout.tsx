0c4023ec3b61731cecc25baf2566e7ef0f5f6e82
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2021-02-03 14:58:25 +0100

    loggedUser hook wip

--- client/components/layout/Layout.tsx
+++ client/components/layout/Layout.tsx
@@ -1,17 +1,10 @@
+import React from "react";
 import Footer from "./Footer";
 import Header from "./header/Header";
 import {Box, Container} from "@material-ui/core";
-import React, {useEffect} from "react";
 import Toast from "./Toast";
-import useLoggedUser from "../../hooks/useLoggedUser";
 
 const Layout = ({children}) => {
-	const loggedUser = useLoggedUser();
-
-	useEffect(() => {
-		loggedUser();
-	}, []);
-
 	return (
 		<Box
 			display="flex"
--- client/components/layout/header/AuthSection.tsx
+++ client/components/layout/header/AuthSection.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useSelector} from 'react-redux';
 import MenuLink from "./MenuLink";
 import {getLoggedUser} from "../../../state/auth/authSelector";
 import {Button} from "@material-ui/core";
 import {IAuthState} from "../../../state/auth/authSlice";
+import useLoggedUser from "../../../hooks/useLoggedUser";
 
 const AuthSection = () => {
     const user: IAuthState = useSelector(getLoggedUser);
+    const loggedUser = useLoggedUser();
+
+    useEffect(() => {
+        loggedUser();
+    }, [user]);
 
     if (user) {
         return (
