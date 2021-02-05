e0ca3f40ad3bba6d156cf9904725128716de185f
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2021-02-05 11:50:10 +0100

    add cors to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 5001;
 
+// var whitelist = ['http://localhost:3001/', 'localhost']
+// var corsOptions = {
+// 	origin: function (origin, callback) {
+// 		if (whitelist.indexOf(origin) !== -1) {
+// 			callback(null, true)
+// 		} else {
+// 			callback(new Error('Not allowed by CORS'))
+// 		}
+// 	}
+// }
+
+app.use(cors())
+
 app.get('/', (req, res) => {
-	res.header('Access-Control-Allow-Origin', '*');
 	res.send(`Hello World from NodeJS server! (http://localhost:${port})`)
 });
 
